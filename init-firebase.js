947fb182f1cadf441b87bb88879f27699af541f8
kennyycodes <kennedyharrison2006@gmail.com>
2023-08-14 09:31:20 -0400

    first commit

---
+++ init-firebase.js
@@ -0,0 +1,16 @@
+/**
+ * @TODO replace the firebaseConfig object below with the one 
+ * provided for you by firebase after creating your project!
+*/
+const firebaseConfig = {
+  apiKey: "AIzaSyDUlCnER-wQDaopOXy0K8fMTNGkJKbNr9w",
+  authDomain: "fir-blog-598cb.firebaseapp.com",
+  projectId: "fir-blog-598cb",
+  storageBucket: "fir-blog-598cb.appspot.com",
+  messagingSenderId: "261024785388",
+  appId: "1:261024785388:web:73b0a340ff074315b416c9",
+  measurementId: "G-H50KH57YP1"
+};
+
+// initialize the firebase app with this built-in function
+firebase.initializeApp(firebaseConfig);
